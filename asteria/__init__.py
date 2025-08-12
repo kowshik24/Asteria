671799012c4d258a4cc2c2995eae4a19c2310e90
kowshik24 <kowshikcseruet1998@gmail.com>
2025-08-12 20:37:19 +0600

    Refactor Asteria API: replace problematic import with actual modules and update __all__ exports

--- asteria/__init__.py
+++ asteria/__init__.py
@@ -1,2 +1,17 @@
 # Placeholder Python API via pybind11 bindings (to be implemented)
-from ._asteria import Index, IndexParams
+# Remove the problematic import and add the actual modules
+from .bor import ButterflyRotation
+from .ecvh import ECVH
+from .lrsq import LRSQ
+from .index_cpu import AsteriaIndexCPU
+from .utils import mine_pairs, hamming_neighbors, pack_bits
+
+__all__ = [
+    "ButterflyRotation",
+    "ECVH", 
+    "LRSQ",
+    "AsteriaIndexCPU",
+    "mine_pairs",
+    "hamming_neighbors", 
+    "pack_bits"
+]
